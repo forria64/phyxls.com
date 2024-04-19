93f05b996ab17985cd5b947da35741ccf7ea0e22
forria <forria@forria64.space>
2024-04-19 22:10:12 +0300

    Added pages, modified menu

--- js/drop_down.js
+++ js/drop_down.js
@@ -18,5 +18,36 @@ document.addEventListener('DOMContentLoaded', function() {
       isOpen = false;
     }
   });
+
+  // Add event listener to toggle team dropdown
+  const teamButton = document.getElementById('team-button');
+  const teamDropdown = document.getElementById('team-dropdown');
+  teamButton.addEventListener('click', function() {
+    teamButton.classList.toggle('active');
+    teamDropdown.classList.toggle('active');
+  });
+
+  // Open submenu on specific pages
+  const currentPage = window.location.pathname;
+  const pagesToOpenSubmenu = ['/stefanb.html', '/phovel.html', '/forria.html'];
+  if (pagesToOpenSubmenu.includes(currentPage)) {
+    teamButton.classList.add('active');
+    teamDropdown.classList.add('active');
+  }
+
+  // Handle click event on "team" link
+  const teamLink = document.getElementById('team-link');
+  teamLink.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default action
+    menu.style.transform = 'translateX(0)'; // Open the menu
+    menuIcon.src = '/images/open.png'; // Change icon to open.png
+    isOpen = true; // Update isOpen flag
+    teamButton.classList.add('active'); // Extend submenu
+    teamDropdown.classList.add('active'); // Extend submenu
+  });
 });
+
+function navigateTo(url) {
+  window.location.href = url;
+}
 
