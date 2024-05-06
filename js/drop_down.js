07e82122eb91999c6e9e3c02036037ea7d68ddc2
forria <forria@forria64.space>
2024-05-06 17:04:00 +0300

    tweaked menu behaviour, removed hover effects on mobile devices

--- js/drop_down.js
+++ js/drop_down.js
@@ -1,67 +1,94 @@
 document.addEventListener('DOMContentLoaded', function() {
   const menuIcon = document.getElementById('menu-icon');
   const menu = document.getElementById('menu');
-  let isOpen = false;
+  let isOpen = localStorage.getItem('menuOpen') === 'true';
 
-  // Initially hide the menu
-  menu.style.transform = 'translateX(100%)';
-  menuIcon.src = '/images/closed.png'; // Set the initial icon to closed.png
+  // Function to open the menu
+  function openMenu() {
+    menuIcon.src = '/images/open.png'; // Change icon to open.png
+    menu.style.transform = 'translateX(0)';
+    isOpen = true;
+    localStorage.setItem('menuOpen', 'true'); // Store menu state
+  }
 
-  menuIcon.addEventListener('click', function() {
-    if (!isOpen) {
-      menuIcon.src = '/images/open.png'; // Change icon to open.png when opening the menu
-      menu.style.transform = 'translateX(0)';
-      isOpen = true;
+  // Function to close the menu
+  function closeMenu() {
+    menuIcon.src = '/images/closed.png'; // Change icon to closed.png
+    menu.style.transform = 'translateX(100%)';
+    isOpen = false;
+    localStorage.setItem('menuOpen', 'false'); // Store menu state
+  }
+
+  // Function to toggle the menu
+  function toggleMenu() {
+    if (isOpen) {
+      closeMenu();
     } else {
-      menuIcon.src = '/images/closed.png'; // Change icon to closed.png when closing the menu
-      menu.style.transform = 'translateX(100%)';
-      isOpen = false;
+      openMenu();
+    }
+  }
+
+  // Handle click event on menu icon to toggle menu
+  menuIcon.addEventListener('click', function(event) {
+    toggleMenu(); // Toggle the menu
+    event.stopPropagation(); // Prevent click event from propagating to the document body
+  });
+
+  // Function to close the menu when clicking outside of it
+  document.addEventListener('click', function(event) {
+    const isMenuClicked = menu.contains(event.target) || menuIcon.contains(event.target);
+    if (!isMenuClicked && isOpen) {
+      closeMenu();
     }
   });
+
+  // Function to toggle team dropdown
+  function toggleTeamDropdown() {
+    const teamButton = document.getElementById('team-button');
+    const teamDropdown = document.getElementById('team-dropdown');
+    teamButton.classList.toggle('active');
+    teamDropdown.classList.toggle('active');
+  }
+
+  // Function to toggle about dropdown
+  function toggleAboutDropdown() {
+    const aboutButton = document.getElementById('about-button');
+    const aboutDropdown = document.getElementById('about-dropdown');
+    aboutButton.classList.toggle('active');
+    aboutDropdown.classList.toggle('active');
+  }
 
   // Add event listener to toggle team dropdown
   const teamButton = document.getElementById('team-button');
   const teamDropdown = document.getElementById('team-dropdown');
   teamButton.addEventListener('click', function() {
-    teamButton.classList.toggle('active');
-    teamDropdown.classList.toggle('active');
+    toggleTeamDropdown();
   });
 
   // Add event listener to toggle about dropdown
   const aboutButton = document.getElementById('about-button');
   const aboutDropdown = document.getElementById('about-dropdown');
   aboutButton.addEventListener('click', function() {
-    aboutButton.classList.toggle('active');
-    aboutDropdown.classList.toggle('active');
+    toggleAboutDropdown();
   });
 
   // Open submenu on specific pages for Team
   const currentPage = window.location.pathname;
   const pagesToOpenTeamSubmenu = ['/forria', '/phovel', '/stefanb'];
   if (pagesToOpenTeamSubmenu.includes(currentPage)) {
-    teamButton.classList.add('active');
-    teamDropdown.classList.add('active');
+    openMenu();
+    toggleTeamDropdown();
   }
 
   // Open submenu on specific pages for About
-  const pagesToOpenAboutSubmenu = ['/concept', '/white_canvas', '/digital_processing', '/image_properties', '/traits', '/reveal_game'];
+  const pagesToOpenAboutSubmenu = ['/digitization', '/metadata', '/phyxls_1'];
   if (pagesToOpenAboutSubmenu.includes(currentPage)) {
-    aboutButton.classList.add('active');
-    aboutDropdown.classList.add('active');
+    openMenu();
+    toggleAboutDropdown();
   }
-
-  // Handle click event on "team" link
-  const teamLink = document.getElementById('team-link');
-  teamLink.addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent default action
-    menu.style.transform = 'translateX(0)'; // Open the menu
-    menuIcon.src = '/images/open.png'; // Change icon to open.png
-    isOpen = true; // Update isOpen flag
-    teamButton.classList.add('active'); // Extend submenu
-    teamDropdown.classList.add('active'); // Extend submenu
-  });
 });
 
 function navigateTo(url) {
   window.location.href = url;
 }
+
